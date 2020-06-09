4dcaa8ed5c80ead1fb8cf463e22959b93acc98ec
liperubo <liperubo@gmail.com>
2020-06-09 17:48:38 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import http from 'http'
+import createGame from './public/game.js'
+import socketio from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+const sockets = socketio(server)
+
+app.use(express.static('public'))
+
+const game = createGame()
+
+game.subscribe((command) => {
+    console.log(`> Emitting ${command.type}`)
+    sockets.emit(command.type, command)
+})
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id
+    console.log(`> Player connected on Server with id: ${playerId}`)
+
+    game.addPlayer({playerId})
+
+    socket.emit('setup', game.state)
+
+    socket.on('disconnect', () => {
+        game.removePlayer({playerId})
+        console.log(`> Player disconnected from the Server with id: ${playerId}`)
+    })
+
+    socket.on('move-player', (command) =>{
+        command.playerId = playerId
+        game.movePlayer(command)
+    })
+})
+
+server.listen(3000, () => {
+    console.log(`> Server listening on port: 3000`)
+})
